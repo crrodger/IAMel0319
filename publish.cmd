7029ef6bdd185c6e8cdff7dc6c2153556cec3a99
Craig Rodger <crrodger@microsoft.com>
2019-03-29 14:27:57 +1100

    Inital load

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBotWithCounter.sln -p:DeployOnBuild=true -p:PublishProfile=picturebotcer-Web-Deploy.pubxml -p:Password=M8FqKFo7v0AkaTPyHAMsX0cDM3xaMBHrcgo2vtQr8qPf96A2nlWSlmdcnyJa
+
